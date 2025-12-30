58524193f705e1fff97ff81a24b6826477756cb7
devopspractice08 <shaikhtaufikh945@gmail.com>
2025-12-30 17:34:50 +0530

    Update trivy-docker-image-scan.sh

--- trivy-docker-image-scan.sh
+++ trivy-docker-image-scan.sh
@@ -5,19 +5,18 @@ IMAGE="shaikh7/numeric-app:${GIT_COMMIT}"
 
 echo "Scanning image: $IMAGE"
 
-# Trivy cache directory (isolated, safe)
 mkdir -p /tmp/trivy-cache
 
-# HIGH vulnerabilities (do not fail build)
 docker run --rm \
+  -v /var/run/docker.sock:/var/run/docker.sock \
   -v /tmp/trivy-cache:/root/.cache/ \
   aquasec/trivy:0.52.0 \
-  image --severity HIGH --exit-code 0 $IMAGE
+  image --scanners vuln --severity HIGH --exit-code 0 $IMAGE
 
-# CRITICAL vulnerabilities (fail build)
 docker run --rm \
+  -v /var/run/docker.sock:/var/run/docker.sock \
   -v /tmp/trivy-cache:/root/.cache/ \
   aquasec/trivy:0.52.0 \
-  image --severity CRITICAL --exit-code 1 $IMAGE
+  image --scanners vuln --severity CRITICAL --exit-code 1 $IMAGE
 
-echo "Trivy scan completed successfully"
+echo "Trivy scan completed"
